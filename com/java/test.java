8fa125b09d3e999d1dbff8424ddc70cfc8d68b2e
ZYP <HuaXia@itcast.cn>
2022-09-24 21:45:05 +0800

    第一次先把文件git-demo提交到本地库

---
+++ com/java/test.java
@@ -0,0 +1,21 @@
+/**
+ * @author:张鹏
+ * @description: 测试克隆代码，以及远程推送
+ * @date: 2022/9/24 21:10
+ */
+public class test {
+    public static void main(String[] args) {
+//        yy 是复制 p是粘贴 在打字的的时候按innsert  在执行命令的时候要按esc回退一下
+//        按:wq 保存文件  vim hello.txt 创建文件 或修改文件
+//                git的语法和Linux一样的
+//        cat xx.txt 在git界面查看该文本类容
+//        当修改完文件后 要添加到缓存区 git add
+//        之后在可以提交  git commit了 但起个名字
+//
+//        一般vim修改完后 都要添加栈存区 git add xx.txt
+//        后要 保存到本地库resposity git commit "+名字" xx.text
+//        git 复制粘贴 ctrl+ins shift+ins
+//
+//        使用git log h或git log path/to/后 git一直停留在log模式,这时只需要按q 键即可退
+    }
+}
